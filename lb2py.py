b2738597a91ab287602bfb08f18fe52a8184bc2a
VKlimenko <fiq2000fiq@gmail.com>
2024-10-06 17:45:13 +0300

    first-commit-laboratory2-del_prints

--- lb2py.py
+++ lb2py.py
@@ -32,24 +32,17 @@ for n in range(len(products)):
         shoplist.append((shops[i], prise))
     all_price.append(shoplist)
 
-print(all_price)
-print(all_price[0])
-print(all_price[0][1])
-print(all_price[0][1][0])
-
 low_shop = []
 
 for x, i in enumerate(all_price):
-    print(x, i)
+
     only_shop = i[1:]
     sum_one_shop = 0
     for y in only_shop:
-        print(y[1])
+
         sum_one_shop += y[1]
     low_shop.append([shops[x], sum_one_shop])
 
-print()
-print(low_shop)
 low_shop.sort(key=lambda x: x[1])
 low_shop_one = low_shop[0]
 print(f'Самый выгодный магазин для покупки: {low_shop_one[0]} - {low_shop_one[1]}руб')
